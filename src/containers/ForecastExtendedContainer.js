cc88ca81e9bd31c48a340fa13ec2e2eb30f54899
Mario Ochoa <mario8ato@hotmail.com>
2020-05-18 20:22:24 -0500

    CreateSelector

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import  {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { getForecastDataFromCities } from '../reducers/cities';
+import { getForecastDataFromCities, getCity } from '../reducers';
 
 import ForecastExtended from '../components/ForecastExtended';
 
@@ -20,6 +20,6 @@ ForecastExtendedContainer.propTypes = {
     city: PropTypes.string.isRequired,
     forecastData: PropTypes.array,
 };
-const mapStateToProps = state => ({city: state.city , forecastData: getForecastDataFromCities(state.cities, state.city) });
+const mapStateToProps = state => ({city: getCity(state) , forecastData: getForecastDataFromCities(state) });
 
 export default connect(mapStateToProps, null )(ForecastExtendedContainer);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
-import {cities} from './cities';
+import {createSelector} from 'reselect';
+import {cities, getForecastDataFromCities as _getForecastDataFromCities} from './cities';
 import {city} from './city';
 
 
@@ -7,4 +8,8 @@ import {city} from './city';
 export default combineReducers({
     cities,
     city
-})
+});
+
+export const getCity = createSelector(state => state.city, city => city);
+
+export const getForecastDataFromCities = createSelector(state => state.cities, getCity, _getForecastDataFromCities);
